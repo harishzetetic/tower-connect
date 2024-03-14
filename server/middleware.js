2f49b402f8778e7b81e23b1998248389df37d197
Harish Sharma <harish.zetetic@gmail.com>
2024-03-14 21:51:24 +0530

    Vercel Error Fix

--- server/middleware.js
+++ server/middleware.js
@@ -1,10 +1,10 @@
-import jwt from 'jsonwebtoken';
+import {verify} from 'jsonwebtoken';
 
 export const verifyAuthorization = (request, response, next) => {
     const authHeader = request.headers['authorization'];
     if (authHeader.startsWith('Bearer ')) {
         const token = authHeader.substring(7, authHeader.length);
-        jwt.verify(token, process.env.JWT_SECRETKEY, (err, data) => {
+        verify(token, process.env.JWT_SECRETKEY, (err, data) => {
             if(err){
                 console.log('❌ Token Invalid')
                 return response.status(401).json({result: "Invalid Token", isTokenValid: false})
